6834258784888520d3493d2e8d0f568b176803b2
karthikq <kk968346@gmail.com>
2022-03-05 11:40:44 +0530

    create

--- server/routes/mainRoute.js
+++ server/routes/mainRoute.js
@@ -4,15 +4,22 @@ const express = require("express");
 const route = express.Router();
 const db = require("../database/index");
 
+route.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 route.post("/contact", async (req, res) => {
   const { first_name, last_name, email, message } = req.body;
 
-  const newContact = await db.query(
-    "INSERT INTO contact_details(first_name,last_name,email,message) VALUES($1,$2,$3,$4) RETURNING *",
-    [first_name, last_name, email, message]
-  );
-
-  res.send({ data: newContact.rows[0] });
+  try {
+    const newContact = await db.query(
+      "INSERT INTO contact_details(first_name,last_name,email,message,created_at) VALUES($1,$2,$3,$4,CURRENT_TIMESTAMP) RETURNING *",
+      [first_name, last_name, email, message]
+    );
+    res.send({ data: newContact.rows[0] });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = route;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,6 @@ app.use(express.json());
 
 app.use("/", require("./routes/mainRoute"));
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("server is running");
 });
