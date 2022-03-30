a370b672de66da854da0a3406357652a1b25da5f
karthikq <kk968346@gmail.com>
2022-03-30 13:24:33 +0530

    ch

--- server/routes/mainRoute.js
+++ server/routes/mainRoute.js
@@ -3,6 +3,16 @@
 const express = require("express");
 const route = express.Router();
 const db = require("../database/index");
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
 
 route.get("/", (req, res) => {
   res.send("Server is running");
@@ -10,16 +20,29 @@ route.get("/", (req, res) => {
 
 route.post("/contact", async (req, res) => {
   const { first_name, last_name, email, message } = req.body;
+  console.log(email);
 
-  try {
-    const newContact = await db.query(
-      "INSERT INTO contact_details(first_name,last_name,email,message,created_at) VALUES($1,$2,$3,$4,CURRENT_TIMESTAMP) RETURNING *",
-      [first_name, last_name, email, message]
-    );
-    res.send({ data: newContact.rows[0] });
-  } catch (error) {
-    console.log(error);
-  }
+  transporter
+    .sendMail({
+      from: "karthikemail99@gmail.com",
+      to: email,
+      subject: "contact@karthikjs",
+      html: "<h2>Thanks for contacting Karthik</h2> <p>I will be in contact with you shortly</p> <p>Thanks</p>",
+    })
+    .then(async (res) => {
+      try {
+        const newContact = await db.query(
+          "INSERT INTO contact_details(first_name,last_name,email,message,created_at) VALUES($1,$2,$3,$4,CURRENT_TIMESTAMP) RETURNING *",
+          [first_name, last_name, email, message]
+        );
+        res.send({ data: newContact.rows[0] });
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = route;
